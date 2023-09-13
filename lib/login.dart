15dd96d14e680f4a86d4f30f2870e3debad3dcf4
Aneeta George <aneetageorge34@gmail.com>
2023-09-13 14:50:57 -0700

    Added username and password input field along with login button.

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-  const Login({super.key});
+  Login({super.key});
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void login()
+  {
+    print('Username: ${usernameController.text}');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +43,31 @@ class Login extends StatelessWidget {
               ),
               Image.network(
                 'https://www.olark.com/img/views/index/howitworks/ChatinRealTime_img.jpg',
-              )
+                height: 200,
+              ),
+
+              TextField(
+                controller: usernameController,
+                decoration: const InputDecoration(
+                    hintText: 'Username',
+                    border: OutlineInputBorder()
+                ),
+              ),
+
+              TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder()
+                ),
+              ),
+
+              ElevatedButton(
+                onPressed: login,
+                child: const Text(
+                  'Login'
+                ))
             ],
           ),
         )
